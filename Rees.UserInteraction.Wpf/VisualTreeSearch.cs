35aefb4267a239ec9ec0eb852df9f9602f080141
Benrnz <Ben@Rees.biz>
2014-01-25 11:23:04 +1300

    Add ability to search for a wpf visual tree child of a certain type

---
+++ Rees.UserInteraction.Wpf/VisualTreeSearch.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Media;
+
+namespace Rees.Wpf
+{
+    public class VisualTreeSearch
+    {
+        public T GetVisualChild<T>(Visual parent) where T : Visual
+        {
+            T child = default(T);
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+
+            for (int i = 0; i < numVisuals; i++)
+            {
+                var v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null)
+                {
+                    child = GetVisualChild<T>(v);
+                }
+                if (child != null)
+                {
+                    break;
+                }
+            }
+            return child;
+        }
+    }
+}
