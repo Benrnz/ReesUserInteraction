01d94d7d1f9af81eda09ec434b7eae4ab7ac8348
Benrnz <Ben@Rees.biz>
2014-02-16 10:31:19 +1300

    Added Text to Static Resource converter

---
+++ Rees.UserInteraction.Wpf/Converters/TextToStaticResourceConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Rees.Wpf.Converters
+{
+    public class TextToStaticResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var resourceName = value as string;
+            if (string.IsNullOrWhiteSpace(resourceName))
+            {
+                return null;
+            }
+
+            object returnValue = Application.Current.TryFindResource(resourceName);
+            return returnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
