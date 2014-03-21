e8b5e720ea37d22e432c5d0129e3563653d1c031
Benrnz <Ben@Rees.biz>
2014-03-21 22:38:57 +1300

    Add converter ZeroToVisibilityConverter to convert 0 into Hidden. Useful for hiding and showing CR/DR labels.

--- Rees.UserInteraction.Wpf/Converters/ZeroToVisibilityConverter.cs
+++ Rees.UserInteraction.Wpf/Converters/ZeroToVisibilityConverter.cs
@@ -1,42 +1,44 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Rees.Wpf.Converters
 {
     /// <summary>
-    /// A Converter that will convert any type (decimal, double, int etc) of zero into Null and leave non-zero intact.
+    /// A Converter that will convert any type (decimal, double, int etc) of zero into <see cref="Visibility.Collapsed"/> and 
+    /// non-zero into <see cref="Visibility.Visible"/>.
     /// </summary>
-    public class ZeroToBlankConverter : IValueConverter
+    public class ZeroToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
             {
-                return null;
+                return Visibility.Hidden;
             }
 
             if (value is decimal && (decimal) value == 0)
             {
-                return null;
+                return Visibility.Hidden;
             }
 
             if (value is double && (double) value == 0)
             {
-                return null;
+                return Visibility.Hidden;
             }
 
             if (value is int && (int) value == 0)
             {
-                return null;
+                return Visibility.Hidden;
             }
 
             if (value is long && (long) value == 0)
             {
-                return null;
+                return Visibility.Hidden;
             }
 
-            return value;
+            return Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
