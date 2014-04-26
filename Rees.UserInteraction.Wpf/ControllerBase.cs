9536d8c4f0a15a1e91fbaecd07e88341ab77ede5
benrnz <ben@rees.biz>
2014-04-27 08:11:24 +1200

    Added MultiBoolToVisibilityConverter

--- Rees.UserInteraction.Wpf/ControllerBase.cs
+++ Rees.UserInteraction.Wpf/ControllerBase.cs
@@ -9,23 +9,18 @@ namespace Rees.Wpf
         private readonly Dispatcher doNotUseDispatcher;
         // ReSharper disable FieldCanBeMadeReadOnly.Local
         // Required for testing
-        private IMessenger doNotUseMessenger = GalaSoft.MvvmLight.Messaging.Messenger.Default;
         // ReSharper restore FieldCanBeMadeReadOnly.Local
 
         public ControllerBase()
         {
             // This relies on the Xaml being responsible for instantiating the controller.
+            // Or at least the main UI thread.
             this.doNotUseDispatcher = Dispatcher.CurrentDispatcher;
         }
 
         protected Dispatcher Dispatcher
         {
             get { return this.doNotUseDispatcher; }
-        }
-
-        protected IMessenger Messenger
-        {
-            get { return this.doNotUseMessenger; }
         }
     }
 }
---
+++ Rees.UserInteraction.Wpf/Converters/MultiBoolToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Rees.Wpf.Converters
+{
+    public class MultiBoolToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            try
+            {
+                return values.Cast<bool>().All(v => v) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            catch (InvalidCastException)
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
